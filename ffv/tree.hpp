78b68632f529a373e4e1c1f841af13c9a8eebbfb
Felix Jones <felix@felixjones.co.uk>
2020-09-05 00:52:46 +0200

    Massaging code of tree.hpp

--- ffv/tree.hpp
+++ ffv/tree.hpp
@@ -1,6 +1,7 @@
 #ifndef FFV_TREE_HPP
 #define FFV_TREE_HPP
 
+#include <algorithm>
 #include <optional>
 #include <vector>
 
@@ -8,6 +9,8 @@ namespace ffv {
 
 template <class KeyType, class ValueType>
 class tree {
+protected:
+	class node;
 public:
 	using size_type = std::uint32_t;
 	using key_type = KeyType;
@@ -16,7 +19,7 @@ public:
 	class iterator {
 		friend tree;
 	public:
-		explicit iterator( tree& owner, size_type pos ) noexcept : m_owner { owner }, m_pos { pos } {}
+		explicit iterator( tree& owner, const size_type pos ) noexcept : m_owner { owner }, m_pos { pos } {}
 
 		bool operator ==( const iterator& other ) const noexcept {
 			return &m_owner == &other.m_owner && m_pos == other.m_pos;
@@ -26,16 +29,16 @@ public:
 			return &m_owner != &other.m_owner || m_pos != other.m_pos;
 		}
 
-		class node& operator *() noexcept {
+		node& operator *() noexcept {
 			return m_owner.m_nodes[m_pos];
 		}
 
-		class node * operator->() noexcept {
+		node * operator ->() noexcept {
 			return &m_owner.m_nodes[m_pos];
 		}
 
 		iterator& find_next( const key_type& key ) noexcept {
-			auto node = m_owner.m_nodes[m_pos].find( key );
+			const auto node = m_owner.m_nodes[m_pos].find( key );
 			m_pos = node.m_pos;
 			return *this;
 		}
@@ -55,15 +58,23 @@ public:
 			return &m_owner == &other.m_owner && m_pos == other.m_pos;
 		}
 
+		bool operator ==( const iterator& other ) const noexcept {
+			return &m_owner == &other.m_owner && m_pos == other.m_pos;
+		}
+
 		bool operator !=( const const_iterator& other ) const noexcept {
 			return &m_owner != &other.m_owner || m_pos != other.m_pos;
 		}
 
-		const class node& operator *() const noexcept {
+		bool operator !=( const iterator& other ) const noexcept {
+			return &m_owner != &other.m_owner || m_pos != other.m_pos;
+		}
+
+		const node& operator *() const noexcept {
 			return m_owner.m_nodes[m_pos];
 		}
 
-		const class node * operator->() const noexcept {
+		const node * operator ->() const noexcept {
 			return &m_owner.m_nodes[m_pos];
 		}
 
@@ -79,9 +90,70 @@ public:
 
 	};
 
+	explicit tree() noexcept : m_nodes( 1, node( this, static_cast<size_type>( 0 ), key_type() ) ) {}
+
+	tree( const tree& other ) noexcept {
+		m_nodes.reserve( other.m_nodes.size() );
+		std::transform( std::cbegin( other.m_nodes ), std::cend( other.m_nodes ), std::back_inserter( m_nodes ),
+			[this]( const node& n ) {
+				return node( this, n );
+			}
+		);
+	}
+
+	bool empty() const noexcept {
+		return m_nodes.size() == 1; // only root
+	}
+
+	iterator begin() const noexcept {
+		return iterator( *this, 0 );
+	}
+
+	iterator end() noexcept {
+		return iterator( *this, static_cast<size_type>( -1 ) );
+	}
+
+	const_iterator cbegin() const noexcept {
+		return const_iterator( *this, 0 );
+	}
+
+	const_iterator cend() const noexcept {
+		return const_iterator( *this, static_cast<size_type>( -1 ) );
+	}
+
+	iterator find( const key_type& key ) noexcept {
+		return m_nodes.front().find( key );
+	}
+
+	const_iterator find( const key_type& key ) const noexcept {
+		return m_nodes.front().find( key );
+	}
+
+	template <class Iter>
+	auto insert( Iter first, Iter last, const value_type& value ) noexcept -> std::enable_if_t<std::is_same_v<typename std::iterator_traits<Iter>::value_type, KeyType>, void> {
+		size_type nodeIndex = 0;
+		while ( first != last ) {
+			const auto next = m_nodes[nodeIndex].find( *first );
+			if ( next == cend() ) {
+				const auto index = static_cast<size_type>( m_nodes.size() );
+				m_nodes[nodeIndex].m_children.push_back( index );
+				m_nodes.emplace_back( this, index, *first );
+				nodeIndex = index;
+			} else {
+				nodeIndex = next.m_pos;
+			}
+			++first;
+		}
+
+		m_nodes[nodeIndex].m_value = value;
+	}
+
+protected:
 	class node {
 		friend tree;
 	public:
+		explicit node( tree * const owner, const size_type index, const key_type& key ) noexcept : m_key { key }, m_value {}, m_owner { owner }, m_index { index }, m_children {} {}
+
 		auto& value() noexcept {
 			return m_value;
 		}
@@ -111,68 +183,18 @@ public:
 		}
 
 	protected:
-		explicit node( tree * owner, const size_type index, const key_type& key ) noexcept : m_owner { owner }, m_index { index }, m_children {}, m_key { key }, m_value {{}} {}
+		explicit node( tree * const owner, const node& other ) noexcept : m_key { other.m_key }, m_value { other.m_value }, m_owner { owner }, m_index { other.m_index }, m_children { other.m_children } {}
 
-	private:
-		tree *					m_owner;
-		const size_type			m_index;
-		std::vector<size_type>	m_children;
-
-	protected:
 		const key_type				m_key;
 		std::optional<value_type>	m_value;
 
+	private:
+		tree * const			m_owner;
+		const size_type			m_index;
+		std::vector<size_type>	m_children;
+
 	};
 
-	explicit tree() noexcept : m_nodes( 1, node( this, static_cast<size_type>( 0 ), key_type() ) ) {}
-
-	tree( const tree& other ) noexcept : m_nodes( other.m_nodes ) {
-		for ( auto& node : m_nodes ) {
-			node.m_owner = this;
-		}
-	}
-
-	auto empty() const noexcept {
-		return m_nodes.size() == 1; // only root
-	}
-
-	iterator end() noexcept {
-		return iterator( *this, static_cast<size_type>( -1 ) );
-	}
-
-	const_iterator cend() const noexcept {
-		return const_iterator( *this, static_cast<size_type>( -1 ) );
-	}
-
-	const_iterator cbegin() const noexcept {
-		return const_iterator( *this, 0 );
-	}
-
-	const_iterator find( const key_type& key ) const noexcept {
-		return m_nodes.front().find( key );
-	}
-
-	template <class Iter>
-	auto insert( Iter first, Iter last, const value_type& value ) noexcept -> std::enable_if_t<std::is_same_v<typename Iter::value_type, typename key_type>, void> {
-		auto nodeIndex = 0;
-
-		while ( first != last ) {
-			const auto next = m_nodes[nodeIndex].find( *first );
-			if ( next == end() ) {
-				const auto index = static_cast<size_type>( m_nodes.size() );
-				m_nodes[nodeIndex].m_children.push_back( index );
-				m_nodes.push_back( node( this, index, *first ) );
-				nodeIndex = index;
-			} else {
-				nodeIndex = next.m_pos;
-			}
-			++first;
-		}
-
-		m_nodes[nodeIndex].m_value = value;
-	}
-
-protected:
 	std::vector<node>	m_nodes;
 
 };
